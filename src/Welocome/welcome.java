06f5b3a469c72779516c062f8f94f46e46383ed1
wjl122 <shangtongdexin122@163.com>
2017-07-31 08:25:48 +0800

    Thread

--- src/Welocome/welcome.java
+++ src/Welocome/welcome.java
@@ -3,16 +3,16 @@ package Welocome;
 public class welcome {
 
 	public static void main(String[] arrgs) {
-		/*
-		 * String[] greeting =new String[3]; greeting[0]="welcom to java :";
-		 * greeting[1]="wjl "; greeting[2]=",good! "; for(String h:greeting){
-		 * System.out.print(h); }
-		 */
-		
-		
-		
-		ImageView imageView = new ImageView();
-		imageView.Action();
+
+		// 窗体
+		// ImageView imageView = new ImageView();
+		// imageView.Action();
+
+		// 线程
+		MyThread mt1 = new MyThread("线程A ");
+		MyThread mt2 = new MyThread("线程BB ");
+		mt1.start();
+		mt2.start();
 	}
 
 }
